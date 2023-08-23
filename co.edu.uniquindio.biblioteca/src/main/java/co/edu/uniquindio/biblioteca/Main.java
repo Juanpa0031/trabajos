30b2620a5a02d07f4786761b23d391654974d15a
Juanpa0031 <131221100+Juanpa0031@users.noreply.github.com>
2023-08-22 23:54:54 -0500

    clase libro
    creación de la clase libro

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -1,7 +1,15 @@
 package co.edu.uniquindio.biblioteca;
-
+import co.edu.uniquindio.biblioteca.libro;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola todos!");
+
+
     }
+    libro libro1 = new libro();
+
+    libro libro2 = new libro();
+    libro libro3 = new libro();
+    libro libro4 = new libro();
+    libro libro5 = new libro();
+
 }
